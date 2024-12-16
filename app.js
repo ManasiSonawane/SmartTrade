06da516f94d37da523ecbf42546a5422f39a547a
ManasiSonawane <136892797+ManasiSonawane@users.noreply.github.com>
2024-12-16 20:57:09 +0530

    Add Initial Files

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.use(express.static(path.join(__dirname, "/public")));
 const store = MongoStore.create({
   mongoUrl: MONGO_URL,
   crypto: {
-    secret: "mysupersecretcode",
+    secret: process.env.SECRET,
   },
   touchafter: 24 * 60 * 60,
 });
@@ -57,7 +57,7 @@ store.on("error", function (e) {
 });
 const sessionOptions = {
   store,
-  secret: "mysupersecretcode",
+  secret: process.env.SECRET,
   resave: false,
   saveUninitialized: true,
   cookie: {
